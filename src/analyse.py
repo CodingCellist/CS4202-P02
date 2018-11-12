e8d261fb2480aef19e410b1b37396203f45efb1a
teh6 <thomasdma008@hotmail.com>
2018-11-12 23:12:04 +0000

    Added plotting script. Data doesn't seem too promising though...

---
+++ src/analyse.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import glob
+
+data_pattern = './data/*.csv'
+
+
+def analyse():
+    for file in glob.glob(data_pattern):
+        with open(file) as csvfile:
+            dirname = file.split('/')[-1].split('.')[0]
+            df = pd.read_csv(csvfile)
+            df_named = df\
+                .replace(to_replace=r'build/*', value='', regex=True)\
+                .replace(to_replace='{:s}/'.format(dirname), value='', regex=True)
+            # print(dirname)
+            # print(df_named)
+            # exit(0)
+            means_df = df_named.groupby('program').mean()
+            means_df[means_df['utime'] >= 0.1].plot(title=dirname, kind='bar')
+            means_df[means_df['utime'] < 0.1].plot(title=dirname, kind='bar')
+            plt.show()
+
+
+if __name__ == '__main__':
+    analyse()

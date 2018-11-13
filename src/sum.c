afca984baee2fd617e0f5383a1bb1f9abae5bc46
teh6 <thomasdma008@hotmail.com>
2018-11-13 17:06:37 +0000

    Lets hope this one optimises...

---
+++ src/sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "loops.h"
+#define N 10000
+
+unsigned long int sum(unsigned long int n)
+{
+    unsigned long int s = 0;
+    for (unsigned long int i = 1; i <= n; i++)
+    {
+        s += i;
+    }
+    return s;
+}
+
+void main(void)
+{
+    for (int i = 0; i < LOOPS; i++)
+    {
+        unsigned long s = sum(N);
+        printf("%d\n", s);
+    }
+}
+

6feed7b875a5749930b64dc2184167f63235303d
teh6 <thomasdma008@hotmail.com>
2018-11-14 10:39:21 +0000

    Matrices as 1d-array, to see if that makes a difference.

---
+++ src/1d_mmult.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define LOOPS 5
+#define DIM 1024
+
+void printMatrix(int n, int* m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ",  m[i * n + j]);
+        }
+        printf("\n");
+    }
+}
+
+int* makeMatrix(int n)
+{
+    int* matrix = (int*) malloc(n * n * sizeof(int));
+    for (int i = 0; i < n * n; i++)
+    {
+        matrix[i] = rand();
+    }
+    return matrix;
+}
+
+void mmult(int* result, int* a, int* b, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < n; k++)
+            {
+                sum += a[i * n + k] * b[k * n + j];
+            }
+            result[i * n + j] = sum;
+        }
+    }
+}
+
+int main(void)
+{
+    int* a = makeMatrix(DIM);
+    int* b = makeMatrix(DIM);
+    int* result = makeMatrix(DIM);
+    for (int l = 0; l < LOOPS; l++)
+    {
+        mmult(result, a, b, DIM);
+        printf("%d\n", result[42]);
+    }
+    return 0;
+}

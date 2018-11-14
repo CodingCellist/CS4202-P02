a000f71bf4bab64c939f8e4e2a053c26bb20b08c
teh6 <thomasdma008@hotmail.com>
2018-11-14 17:00:47 +0000

    Moved files to allow multiple optimisations.

--- man-inline-src/analyse.py
+++ man-inline-src/analyse.py
@@ -19,19 +19,19 @@ def analyse(show=False):
                 .replace(to_replace='{:s}'.format(dirname[1:]), value='', regex=True)
             means_df = df_named.groupby('program').mean()
             sem_df = df_named.groupby('program').sem()
-            means_df[means_df['utime'] >= 2] \
+            means_df[means_df['utime'] >= 4] \
                 .plot(title=dirname,
                       kind='bar',
-                      yerr=sem_df[means_df['utime'] >= 2])
+                      yerr=sem_df[means_df['utime'] >= 4])
             plt.ylabel(ylabel=ylabel, rotation=90)
             plt.tight_layout()
             if not show:
                 plt.savefig(plot_dest.format(dirname, 0))
-            means_df[(means_df['utime'] < 2) & (means_df['utime'] >= 0.1)] \
+            means_df[(means_df['utime'] < 4) & (means_df['utime'] >= 0.1)] \
                 .plot(title=dirname,
                       kind='bar',
                       yerr=sem_df[
-                          (means_df['utime'] < 2) & (means_df['utime'] >= 0.1)])
+                          (means_df['utime'] < 4) & (means_df['utime'] >= 0.1)])
             plt.ylabel(ylabel=ylabel, rotation=90)
             plt.tight_layout()
             if not show:

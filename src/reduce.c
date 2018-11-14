af3cd0ea727b51802372e479c30a7ecdd961e3de
teh6 <thomasdma008@hotmail.com>
2018-11-14 11:02:45 +0000

    Yet another test case.

---
+++ src/reduce.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "loops.h"
+
+int main(void)
+{
+    const int length = 16384;
+    long int reduction = 0;
+    int[length] arr;
+    for (int l = 0; l < LOOPS; l++)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            reduction += arr[i];
+        }
+        printf("%l\n", reduction);
+    }
+    return 0;
+}
+    
